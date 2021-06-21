c76099a813d3105a5a1ebb4cbb202f31b2cb2d30
dhino12 <dhinorahmad0@gmail.com>
2021-06-21 19:05:28 +0700

    final simple web css/html/js

--- assets/script.js
+++ assets/script.js
@@ -4,3 +4,12 @@ console.log(icon_up);
 icon_up.addEventListener("click", () => {
     window.scrollTo(0,0);
 });
+
+let titleGames = document.querySelectorAll(".card-game");
+
+for (const title of titleGames) {
+    title.addEventListener("click", () => {
+        console.log(title.getElementsByTagName("h2")[0]);
+        alert(`Anda memilih game ${title.getElementsByTagName("h2")[0].innerText}`)
+    })
+}
